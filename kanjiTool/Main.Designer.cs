8f3b667a87df4b082ce923064e5418553f35b73b
qi rui yang <1844812067@qq.com>
2016-10-27 17:32:11 +0800

    添加项目文件。

---
+++ kanjiTool/Main.Designer.cs
@@ -0,0 +1,191 @@
+﻿namespace kanziTool
+{
+    partial class Main
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Main));
+            this.tabControl1 = new System.Windows.Forms.TabControl();
+            this.tabPage1 = new System.Windows.Forms.TabPage();
+            this.button1 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.tabPage2 = new System.Windows.Forms.TabPage();
+            this.label2 = new System.Windows.Forms.Label();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.notifyIcon1 = new System.Windows.Forms.NotifyIcon(this.components);
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.tabControl1.SuspendLayout();
+            this.tabPage1.SuspendLayout();
+            this.tabPage2.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // tabControl1
+            // 
+            this.tabControl1.Controls.Add(this.tabPage1);
+            this.tabControl1.Controls.Add(this.tabPage2);
+            this.tabControl1.Location = new System.Drawing.Point(12, 0);
+            this.tabControl1.Name = "tabControl1";
+            this.tabControl1.SelectedIndex = 0;
+            this.tabControl1.Size = new System.Drawing.Size(324, 126);
+            this.tabControl1.TabIndex = 0;
+            // 
+            // tabPage1
+            // 
+            this.tabPage1.Controls.Add(this.button1);
+            this.tabPage1.Controls.Add(this.label1);
+            this.tabPage1.Controls.Add(this.textBox1);
+            this.tabPage1.Location = new System.Drawing.Point(4, 22);
+            this.tabPage1.Name = "tabPage1";
+            this.tabPage1.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage1.Size = new System.Drawing.Size(316, 100);
+            this.tabPage1.TabIndex = 0;
+            this.tabPage1.Text = "搜索";
+            this.tabPage1.UseVisualStyleBackColor = true;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(8, 55);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(302, 23);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "查询";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(6, 13);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(59, 12);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "搜索假名:";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(6, 28);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(304, 21);
+            this.textBox1.TabIndex = 0;
+            // 
+            // tabPage2
+            // 
+            this.tabPage2.Controls.Add(this.label2);
+            this.tabPage2.Location = new System.Drawing.Point(4, 22);
+            this.tabPage2.Name = "tabPage2";
+            this.tabPage2.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage2.Size = new System.Drawing.Size(242, 100);
+            this.tabPage2.TabIndex = 1;
+            this.tabPage2.Text = "设置";
+            this.tabPage2.UseVisualStyleBackColor = true;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("宋体", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.label2.Location = new System.Drawing.Point(3, 31);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(239, 16);
+            this.label2.TabIndex = 0;
+            this.label2.Text = "并没有设置(才不是我懒呢 哼)";
+            // 
+            // timer1
+            // 
+            this.timer1.Enabled = true;
+            this.timer1.Interval = 1000;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // notifyIcon1
+            // 
+            this.notifyIcon1.Icon = ((System.Drawing.Icon)(resources.GetObject("notifyIcon1.Icon")));
+            this.notifyIcon1.Text = "四酱的小工具";
+            this.notifyIcon1.Visible = true;
+            this.notifyIcon1.MouseDoubleClick += new System.Windows.Forms.MouseEventHandler(this.notifyIcon1_MouseDoubleClick);
+            // 
+            // textBox2
+            // 
+            this.textBox2.Cursor = System.Windows.Forms.Cursors.Default;
+            this.textBox2.Location = new System.Drawing.Point(22, 142);
+            this.textBox2.Multiline = true;
+            this.textBox2.Name = "textBox2";
+            this.textBox2.ReadOnly = true;
+            this.textBox2.ScrollBars = System.Windows.Forms.ScrollBars.Both;
+            this.textBox2.Size = new System.Drawing.Size(310, 193);
+            this.textBox2.TabIndex = 1;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(22, 129);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(29, 12);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "Log:";
+            // 
+            // Main
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(348, 347);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.tabControl1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.Fixed3D;
+            this.MaximizeBox = false;
+            this.Name = "Main";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Tool";
+            this.Load += new System.EventHandler(this.Main_Load);
+            this.Resize += new System.EventHandler(this.Main_Resize);
+            this.tabControl1.ResumeLayout(false);
+            this.tabPage1.ResumeLayout(false);
+            this.tabPage1.PerformLayout();
+            this.tabPage2.ResumeLayout(false);
+            this.tabPage2.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TabControl tabControl1;
+        private System.Windows.Forms.TabPage tabPage1;
+        private System.Windows.Forms.TabPage tabPage2;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.NotifyIcon notifyIcon1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Label label3;
+    }
+}
---
+++ kanjiTool/Main.cs
@@ -0,0 +1,168 @@
+﻿using Newtonsoft.Json.Linq;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace kanziTool
+{
+    public partial class Main : Form
+    {
+        public Main()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (textBox1.Text == string.Empty)
+            {
+                MessageBox.Show("警告:请输入字符");
+                return;
+            }
+            string[] info = getKanjiInfo(textBox1.Text);
+            if (info[0] == "ERROR")
+            {
+                MessageBox.Show("获取时出现错误:"+info[1]);
+                return;
+            }
+            string[] translate = getTranslate(textBox1.Text);
+            string mesg = string.Format("漢字[{0}]\r\n假名: {1}\r\n罗马拼音: {2}",textBox1.Text,info[0],(info[1]==string.Empty?"获取失败":info[1]));
+            MessageBox.Show(string.Format("{0}\r\n{1}\r\n警告:以上内容可能不准确,请根据实际情况分析", mesg,translate[0]=="ERROR"?"翻译: 获取失败":translate[0]== "jp"?"翻译: "+translate[1]:"翻译: 获取失败"));
+
+        }
+
+        string backup = string.Empty;
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            if (this.WindowState == FormWindowState.Minimized)
+            {
+                try
+                {
+                    IDataObject iData = Clipboard.GetDataObject();
+                    if (iData.GetDataPresent(DataFormats.Text))
+                    {
+                        if (backup == string.Empty || backup!= (string)iData.GetData(DataFormats.UnicodeText))
+                        {
+                            backup = (string)iData.GetData(DataFormats.UnicodeText);
+                            if (backup.Length > 50)
+                            {
+                                return;
+                            }
+                            string[] info = getKanjiInfo((string)iData.GetData(DataFormats.UnicodeText));
+                            string[] translate = getTranslate((string)iData.GetData(DataFormats.UnicodeText));
+                            string mesg = string.Empty;
+                            if (info[0] == "ERROR")
+                            {
+                                if(translate[0] == "ERROR" || translate[0]!="jp")
+                                {
+                                    return;
+                                }
+                                mesg = "假名: 获取失败  罗马拼音: 获取失败  翻译: "+translate[1];
+                            }
+                            else
+                            {
+                                translate = getTranslate((string)iData.GetData(DataFormats.UnicodeText),"jp");
+                                mesg = string.Format("假名: {1}  罗马拼音: {2}  {3}", textBox1.Text, info[0], (info[1] == string.Empty ? "获取失败" : info[1]), (translate[0] == "ERROR" ? "翻译: 获取失败" : translate[0] == "jp" ? "翻译: " + translate[1] : "翻译: 获取失败"));
+                            }
+                            this.notifyIcon1.ShowBalloonTip(1000, "关于["+ (string)iData.GetData(DataFormats.UnicodeText)+"]:", mesg, ToolTipIcon.None);
+                            this.textBox2.Text += "关于[" + (string)iData.GetData(DataFormats.UnicodeText) + "]:"+mesg + Environment.NewLine;
+                            this.textBox2.Focus();
+                            this.textBox2.Select(this.textBox2.TextLength, 0);
+                            this.textBox2.ScrollToCaret();
+                        }
+                    }
+                }
+                catch(Exception ex)
+                {
+                    this.notifyIcon1.ShowBalloonTip(1000, "出现错误:",ex.ToString(), ToolTipIcon.Info);
+                }
+
+            }
+        }
+
+        string[] getTranslate(string text,string from=null)
+        {
+            try
+            {
+                Random ran = new Random();
+                int RandKey = ran.Next(1000, 9999999);
+                JObject jobj = JObject.Parse(Tool.GetHtml(string.Format("http://api.fanyi.baidu.com/api/trans/vip/translate?q={0}&from={3}&to=zh&appid=20160122000009222&salt={1}&sign={2}",text,RandKey,Tool.StrToMD5("20160122000009222"+text+RandKey+ "EzHTN1B778iYXbKucyAL"),from==null?"auto":from)));
+                JObject jo2 = JObject.Parse(jobj["trans_result"].ToString().Replace("[", string.Empty).Replace("]", string.Empty));
+                return new string[] { (string)jobj["from"], (string)jo2["dst"] };
+            }
+            catch
+            {
+                return new string[] { "ERROR" };
+            }
+        }
+
+        string[] getKanjiInfo(string kanji)
+        {
+            try
+            {
+                string[] array = Tool.GetHtml(string.Format("http://yomikatawa.com/kanji/{0}?search=1", kanji)).Split(new string[] { "\r\n" }, StringSplitOptions.RemoveEmptyEntries);
+                if (array[0] == "ERROR")
+                {
+                    return new string[] { "ERROR", array[1] };
+                }
+                bool flag = false;
+                string[] ret = new string[] { string.Empty,string.Empty};
+                int num = 0;
+                foreach (string line in array)
+                {
+                    if (num == 1)
+                    {
+                        string lm = line.Replace("	", "");
+                        lm = lm.Replace("<p>", "");
+                        lm = lm.Replace("</p>", "");
+                        ret[1] = (lm == "" ? string.Empty : lm);
+                        return ret;
+                    }
+                    if (flag)
+                    {
+                        string hiragana = line.Replace("	", "");
+                        hiragana = hiragana.Replace("<p>", "");
+                        hiragana = hiragana.Replace("</p>", "");
+                        ret[0] = hiragana;
+                        num++;
+                    }
+                    if (line.Contains("<h1>") && line.Contains("/h1") && line.Contains("<strong>") && line.Contains("</strong>"))
+                    {
+                        flag = true;
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                return new string[] { "ERROR", ex.ToString() };
+            }
+            return new string[] { "ERROR", "未知错误" };
+        }
+
+        private void Main_Load(object sender, EventArgs e)
+        {
+            this.notifyIcon1.Visible = true;
+        }
+
+        private void notifyIcon1_MouseDoubleClick(object sender, MouseEventArgs e)
+        {
+            this.Visible = true;
+            this.WindowState = FormWindowState.Normal;
+        }
+
+        private void Main_Resize(object sender, EventArgs e)
+        {
+            if (this.WindowState == FormWindowState.Minimized)
+            {
+                this.Visible = false;
+            }
+        }
+    }
+}
